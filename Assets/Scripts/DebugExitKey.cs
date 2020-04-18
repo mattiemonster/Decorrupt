98ed0eab6491c536a6fae4b730b6855ca9f3b601
Mattie <mattbware1@gmail.com>
2020-04-18 03:29:21 +0100

    Debug exit key

---
+++ Assets/Scripts/DebugExitKey.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DebugExitKey : MonoBehaviour
+{
+    public KeyCode exitKey = KeyCode.Escape;
+
+    void Start()
+    {
+        Debug.Log("Debug exit key enabled!");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(exitKey))
+        {
+            Debug.Log("Debug exit key pressed, quitting.");
+            Application.Quit();
+        }
+    }
+}
